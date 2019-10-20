0760abc75a8fe986ce075fb8890bf46c471840ad
linqian-0525 <1027716017@qq.com>
2019-10-21 00:53:20 +0800

    项目的建立

---
+++ src/main/java/com/lq/blog/handler/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.lq.blog.handler;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+@Slf4j
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHander(HttpServletRequest request, Exception e) throws Exception {
+        log.error("Request URL : {}，Exception : {}", request.getRequestURL(),e);
+
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("url",request.getRequestURL());
+        mv.addObject("exception", e);
+        mv.setViewName("error/error");
+        return mv;
+    }
+}
